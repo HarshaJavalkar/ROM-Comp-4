0a2acd4607c148ebba20d56368809146e5fb886d
harsha javalkar <HarshaJavalkar>
2022-05-31 19:15:10 +0530

    test

--- src/main/java/com/returnordermanag/componentProcessModule/ComponentProcessingMicroserviceApplication.java
+++ src/main/java/com/returnordermanag/componentProcessModule/ComponentProcessingMicroserviceApplication.java
@@ -11,5 +11,4 @@ public class ComponentProcessingMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComponentProcessingMicroserviceApplication.class, args);
 	}
-
 }
